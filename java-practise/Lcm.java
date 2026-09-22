235cda73c20f4a9f308e8c6fc01cf40a05df09e4
tarunvkkaki <tarunvenkatakumarkaki@gmail.com>
2026-09-22 15:48:13 +0000

    finding lcm of a number

---
+++ java-practise/Lcm.java
@@ -0,0 +1,11 @@
+public class Lcm {
+    public int lcm(int a, int b) {
+        int res = Math.max(a, b);
+        while (true) {
+            if (res % a == 0 && res % b == 0) {
+                return res;
+            }
+            res++;
+        }
+    }
+}
---
+++ java-practise/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        Lcm l = new Lcm();
+        System.out.println(l.lcm(4, 6));
+    }
+}
